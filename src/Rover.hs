0f09f5d0d7fbff2c78a10d905afd87710c008555
Andreas Rammhold <andreas@rammhold.de>
2020-06-27 00:33:58 +0200

    This seems to be working

--- src/Rover.hs
+++ src/Rover.hs
@@ -1,4 +1,84 @@
-module Rover () where
+module Rover where
 
-rover :: String
-rover = "rover"
+import Control.Applicative hiding (many)
+import Text.ParserCombinators.ReadP
+import Data.Char
+
+
+data Orientation = N | S | E | W
+   deriving (Show, Eq)
+
+data Instruction = IMove | IRight | ILeft
+   deriving (Show, Eq)
+
+data Coord = Coord { x :: Int, y :: Int }
+   deriving (Show, Eq)
+
+data Rover = Rover Coord Orientation
+   deriving (Show, Eq)
+
+
+orientationReader :: ReadP Orientation
+orientationReader = do
+  c <- char 'E' <|> char 'N'  <|> char 'S'  <|> char 'W'
+  return $ case c of
+    'E' -> E
+    'N' -> N
+    'S' -> S
+    'W' -> W
+
+coordinateReader :: ReadP Coord
+coordinateReader = do
+ x <- number
+ space
+ y <- number
+ return $ Coord (read x) (read y)
+  where
+   number = munch1 isDigit
+   space = char ' '
+
+instructionReader :: ReadP Instruction
+instructionReader = do
+  c <- char 'M' <|> char 'R'  <|> char 'L'
+  return $ case c of
+    'M' -> IMove
+    'L' -> ILeft
+    'R' -> IRight
+
+-- sepBy1 :: ReadP a -> ReadP sep -> ReadP [a]
+
+parseRover :: ReadP Rover
+parseRover = do
+  coords <- coordinateReader
+  char ' '
+  orientation <- orientationReader
+  return $ Rover coords orientation
+
+parseInstructions :: ReadP [Instruction]
+parseInstructions = many instructionReader
+
+
+parseProblem :: ReadP [(Rover, [Instruction])]
+parseProblem = do
+  _ <- coordinateReader
+  char '\n'
+  many $ (do
+    r <- parseRover
+    char '\n'
+    i <- parseInstructions
+    char '\n'
+    pure (r, i))
+  <* eof
+
+moveRover :: Rover -> Instruction -> Rover
+moveRover (Rover c o) ILeft = Rover c (case o of N -> W; W -> S; S -> E; E -> N)
+moveRover (Rover c o) IRight = Rover c (case o of N -> E; E -> S; S -> W; W -> N)
+moveRover (Rover c o) IMove = Rover (case o of
+                                      N -> c { y = y c + 1 }
+                                      S -> c { y = y c - 1 }
+                                      E -> c { x = x c + 1 }
+                                      W -> c { x = x c - 1 }
+                                    ) o
+
+runRover :: Rover -> [Instruction] -> Rover
+runRover = foldl moveRover
--- test/Rover/RoverSpec.hs
+++ test/Rover/RoverSpec.hs
@@ -2,9 +2,64 @@ module Rover.RoverSpec (spec) where
 
 import Test.Hspec
 import Rover
+import Text.ParserCombinators.ReadP
+
+problem :: String
+problem =
+    "5 5\n"
+  ++ "1 2 N\n"
+  ++ "LMLMLMLMM\n"
+
+
+parse :: ReadP a -> String -> Maybe a
+parse p str =
+  case x of
+    [] -> Nothing
+    [(x, _)] -> Just x
+    _ -> Nothing
+  where
+    x = readP_to_S p str
 
 spec :: Spec
 spec = do
-    describe "Rover" $ do
-        it "does something" $ do
-            0 `shouldBe` 0
+  describe "Mars" $ do
+      it "parses orientation correctly" $ do
+        parse orientationReader "E" `shouldBe` Just E
+        parse orientationReader "N" `shouldBe` Just N
+        parse orientationReader "S" `shouldBe` Just S
+        parse orientationReader "W" `shouldBe` Just W
+        parse orientationReader "" `shouldBe` Nothing
+        parse orientationReader "X" `shouldBe` Nothing
+      it "parses coordinate correctly" $ do
+        parse coordinateReader "0 0" `shouldBe` Just (Coord 0 0)
+        parse coordinateReader "10 10" `shouldBe` Just (Coord 10 10)
+        parse coordinateReader "a b" `shouldBe` Nothing
+      it "parses an instruction correctly" $ do
+        parse instructionReader "M" `shouldBe` Just IMove
+        parse instructionReader "L" `shouldBe` Just ILeft
+        parse instructionReader "R" `shouldBe` Just IRight
+      it "parses a Rover correctly" $ do
+        parse parseRover "1 2 N" `shouldBe` (Just (Rover (Coord 1 2) N))
+        parse parseRover "1 X N" `shouldBe` Nothing
+      it "parses instruction lists" $ do
+        parse (parseInstructions <* eof) "MMLR" `shouldBe` Just [IMove, IMove, ILeft, IRight]
+      it "parses problem" $ do
+        parse parseProblem problem `shouldBe` Just [((Rover (Coord 1 2) N), [ILeft, IMove, ILeft, IMove, ILeft, IMove, ILeft, IMove, IMove ])]
+      it "moves the rover" $ do
+        moveRover (Rover (Coord 0 0) N) IMove `shouldBe` Rover (Coord { x = 0, y = 1}) N
+        moveRover (Rover (Coord 0 0) S) IMove `shouldBe` Rover (Coord { x = 0, y = -1}) S
+        moveRover (Rover (Coord 0 0) E) IMove `shouldBe` Rover (Coord { x = 1, y = 0}) E
+        moveRover (Rover (Coord 0 0) W) IMove `shouldBe` Rover (Coord { x = -1, y = 0}) W
+        moveRover (Rover (Coord 0 0) N) IRight `shouldBe` Rover (Coord { x = 0, y = 0}) E
+        moveRover (Rover (Coord 0 0) N) ILeft `shouldBe` Rover (Coord { x = 0, y = 0}) W
+
+      it "runs the rover across mars" $ do
+        runRover (Rover (Coord 0 0) N) [IMove] `shouldBe` Rover (Coord { x = 0, y = 1}) N
+        runRover (Rover (Coord 0 0) N) [ILeft, IMove] `shouldBe` Rover (Coord { x = -1, y = 0}) W
+-- 5 5
+-- 1 2 N
+-- LMLMLMLMM
+--
+-- 3 3 E
+-- MMRMMRMRRM
+--
