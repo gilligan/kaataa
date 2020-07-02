39612aa18b77bf577f9bd9f820f7a187787514db
Andreas Rammhold <andreas@rammhold.de>
2020-07-02 13:41:29 +0200

    Property tests

--- src/Rover.hs
+++ src/Rover.hs
@@ -1,47 +1,51 @@
 module Rover where
 
 import Control.Applicative hiding (many)
-import Text.ParserCombinators.ReadP
 import Data.Char
 import Data.Functor
-
+import Text.ParserCombinators.ReadP
+import System.IO
+import Control.Monad
 
 data Orientation = N | S | E | W
-   deriving (Show, Eq)
+  deriving (Show, Eq, Enum)
 
 data Instruction = IMove | IRight | ILeft
-   deriving (Show, Eq)
+  deriving (Show, Eq)
 
-data Coord = Coord { x :: Int, y :: Int }
-   deriving (Show, Eq)
+data Coord = Coord {x :: Int, y :: Int}
+  deriving (Show, Eq)
 
-data Rover = Rover Coord Orientation
-   deriving (Show, Eq)
+data Rover = Rover { coord :: Coord, orientation :: Orientation }
+  deriving (Show, Eq)
 
+roverDistance :: Rover -> Rover -> Int
+roverDistance (Rover c1 _) (Rover c2 _) = distance c1 c2
+
+distance :: Coord -> Coord -> Int
+distance (Coord x1 y1) (Coord x2 y2) =
+  let x = if x1 > x2 then x1 - x2 else x2 - x1
+      y = if y1 > y2 then y1 - y2 else y2 - y1
+   in abs x + abs y
 
 orientationReader :: ReadP Orientation
 orientationReader =
   (char 'E' $> E)
-  <|>
-  (char 'N' $> N)
-  <|>
-  (char 'S' $> S)
-  <|>
-  (char 'W' $> W)
+    <|> (char 'N' $> N)
+    <|> (char 'S' $> S)
+    <|> (char 'W' $> W)
 
 coordinateReader :: ReadP Coord
 coordinateReader = Coord <$> number <*> (space *> number)
   where
-   number = read <$> munch1 isDigit
-   space = char ' '
+    number = read <$> munch1 isDigit
+    space = char ' '
 
 instructionReader :: ReadP Instruction
 instructionReader =
   (char 'M' $> IMove)
-  <|>
-  (char 'L' $> ILeft)
-  <|>
-  (char 'R' $> IRight)
+    <|> (char 'L' $> ILeft)
+    <|> (char 'R' $> IRight)
 
 parseRover :: ReadP Rover
 parseRover = Rover <$> coordinateReader <*> (char ' ' *> orientationReader)
@@ -49,12 +53,16 @@ parseRover = Rover <$> coordinateReader <*> (char ' ' *> orientationReader)
 parseInstructions :: ReadP [Instruction]
 parseInstructions = many instructionReader
 
+problemReader :: ReadP [(Rover, [Instruction])]
+problemReader = coordinateReader >> char '\n' >> many coordAndInstr <* eof
+  where
+    coordAndInstr = (,) <$> (parseRover <* char '\n') <*> (parseInstructions <* char '\n')
 
-parseProblem :: ReadP [(Rover, [Instruction])]
-parseProblem = coordinateReader >> char '\n' >> (many coordAndInstr) <* eof
- where
-   coordAndInstr = (,) <$> (parseRover <* char '\n') <*> (parseInstructions <* char '\n')
-
+parseProblem :: String -> Maybe [(Rover, [Instruction])]
+parseProblem s = case x of
+                   [(x, _)] -> Just x
+                   _ -> Nothing
+  where x = readP_to_S problemReader s
 
 rotateLeft :: Orientation -> Orientation
 rotateLeft N = W
@@ -71,15 +79,25 @@ rotateRight W = N
 moveRover :: Rover -> Instruction -> Rover
 moveRover (Rover c o) ILeft = Rover c $ rotateLeft o
 moveRover (Rover c o) IRight = Rover c $ rotateRight o
-moveRover (Rover c o) IMove = Rover (case o of
-                                      N -> c { y = y c + 1 }
-                                      S -> c { y = y c - 1 }
-                                      E -> c { x = x c + 1 }
-                                      W -> c { x = x c - 1 }
-                                    ) o
+moveRover (Rover c o) IMove =
+  Rover
+    ( case o of
+        N -> c {y = y c + 1}
+        S -> c {y = y c - 1}
+        E -> c {x = x c + 1}
+        W -> c {x = x c - 1}
+    )
+    o
 
 runRover :: Rover -> [Instruction] -> Rover
 runRover = foldl moveRover
 
 runRovers :: [(Rover, [Instruction])] -> [Rover]
 runRovers = fmap (uncurry runRover)
+
+main :: IO ()
+main = do
+  problem <- parseProblem <$> readFile "input.txt"
+  case problem of
+    Nothing -> print "Invalid input"
+    Just problem -> print $ runRovers problem
--- test/Rover/RoverSpec.hs
+++ test/Rover/RoverSpec.hs
@@ -1,14 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Rover.RoverSpec (spec) where
 
-import Test.Hspec
 import Rover
+import Test.Hspec
+import Test.Hspec.Hedgehog (hedgehog, diff, forAll, failure, (===), cover, Gen, assert)
 import Text.ParserCombinators.ReadP
+import qualified Hedgehog.Range as Range
+import qualified Hedgehog.Gen as Gen
 
 problem :: String
 problem =
-    "5 5\n"
-  ++ "1 2 N\n"
-  ++ "LMLMLMLMM\n"
+  "5 5\n"
+    ++ "1 2 N\n"
+    ++ "LMLMLMLMM\n"
+
+--- 1 2 N
+-- L 1 2 W
+-- M 0 2 W
+-- L 0 2 S
+-- M 0 1 S
+-- L 0 1 E
+-- M 1 1 E
+-- L 1 1 N
+-- M 2 1 N
+-- M 3 1 N
 
 
 parse :: ReadP a -> String -> Maybe a
@@ -20,52 +36,107 @@ parse p str =
   where
     x = readP_to_S p str
 
+
+genCoord :: Gen Coord
+genCoord = do
+  x <- Gen.integral (Range.linear (-100) 100)
+  y <- Gen.integral (Range.linear (-100) 100)
+  pure $ Coord x y
+
+genInstruction :: Gen Instruction
+genInstruction = Gen.choice [ genMoveInstruction, genRotateInstruction ]
+
+genMoveInstruction :: Gen Instruction
+genMoveInstruction = Gen.constant IMove
+
+genRotateInstruction :: Gen Instruction
+genRotateInstruction = Gen.element [ IRight, ILeft ]
+
+genOrientation :: Gen Orientation
+genOrientation = Gen.element $ enumFrom N
+
+genRover :: Gen Rover
+genRover = Rover <$> genCoord <*> genOrientation
+
 spec :: Spec
 spec = do
+
+  describe "movement properties" $ do
+    it "instructions will move the rover by at most 1 unit" $ hedgehog $ do
+      rover <- forAll genRover
+      inst <- forAll genInstruction
+      let r = moveRover rover inst
+      assert (roverDistance rover r <= 1)
+
+    it "rotations do not change the rover coordinates" $ hedgehog $ do
+      rover <- forAll genRover
+      inst <- forAll genRotateInstruction
+      let r = moveRover rover inst
+      assert (roverDistance rover r == 0)
+
   describe "Mars" $ do
-      it "parses orientation correctly" $ do
-        parse orientationReader "E" `shouldBe` Just E
-        parse orientationReader "N" `shouldBe` Just N
-        parse orientationReader "S" `shouldBe` Just S
-        parse orientationReader "W" `shouldBe` Just W
-        parse orientationReader "" `shouldBe` Nothing
-        parse orientationReader "X" `shouldBe` Nothing
-      it "parses coordinate correctly" $ do
-        parse coordinateReader "0 0" `shouldBe` Just (Coord 0 0)
-        parse coordinateReader "10 10" `shouldBe` Just (Coord 10 10)
-        parse coordinateReader "a b" `shouldBe` Nothing
-      it "parses an instruction correctly" $ do
-        parse instructionReader "M" `shouldBe` Just IMove
-        parse instructionReader "L" `shouldBe` Just ILeft
-        parse instructionReader "R" `shouldBe` Just IRight
-      it "parses a Rover correctly" $ do
-        parse parseRover "1 2 N" `shouldBe` (Just (Rover (Coord 1 2) N))
-        parse parseRover "1 X N" `shouldBe` Nothing
-      it "parses instruction lists" $ do
-        parse (parseInstructions <* eof) "MMLR" `shouldBe` Just [IMove, IMove, ILeft, IRight]
-      it "parses problem" $ do
-        parse parseProblem problem `shouldBe` Just [((Rover (Coord 1 2) N), [ILeft, IMove, ILeft, IMove, ILeft, IMove, ILeft, IMove, IMove ])]
-      it "moves the rover" $ do
-        moveRover (Rover (Coord 0 0) N) IMove `shouldBe` Rover (Coord { x = 0, y = 1}) N
-        moveRover (Rover (Coord 0 0) S) IMove `shouldBe` Rover (Coord { x = 0, y = -1}) S
-        moveRover (Rover (Coord 0 0) E) IMove `shouldBe` Rover (Coord { x = 1, y = 0}) E
-        moveRover (Rover (Coord 0 0) W) IMove `shouldBe` Rover (Coord { x = -1, y = 0}) W
-        moveRover (Rover (Coord 0 0) N) IRight `shouldBe` Rover (Coord { x = 0, y = 0}) E
-        moveRover (Rover (Coord 0 0) N) ILeft `shouldBe` Rover (Coord { x = 0, y = 0}) W
+    it "calculates the correct distance" $ do
+      distance (Coord 0 0) (Coord 0 0) `shouldBe` 0
+      distance (Coord 0 0) (Coord 0 1) `shouldBe` 1
+      distance (Coord 0 0) (Coord 1 0) `shouldBe` 1
+      distance (Coord 0 0) (Coord 1 1) `shouldBe` 2
+      distance (Coord 1 1) (Coord 1 1) `shouldBe` 0
+      distance (Coord 1 1) (Coord 1 0) `shouldBe` 1
+      distance (Coord 1 1) (Coord 1 1) `shouldBe` 0
+      distance (Coord 1 1) (Coord 1 (-1)) `shouldBe` 2
+      distance (Coord 1 1) (Coord (-1) (-1)) `shouldBe` 4
+      distance (Coord 1 (-1)) (Coord (-1) (-1)) `shouldBe` 2
+      distance (Coord (-1) (-1)) (Coord (-1) (-1)) `shouldBe` 0
 
-      it "runs the rover across mars" $ do
-        runRover (Rover (Coord 0 0) N) [IMove] `shouldBe` Rover (Coord { x = 0, y = 1}) N
-        runRover (Rover (Coord 0 0) N) [ILeft, IMove] `shouldBe` Rover (Coord { x = -1, y = 0}) W
+    it "parses orientation correctly" $ do
+      parse orientationReader "E" `shouldBe` Just E
+      parse orientationReader "N" `shouldBe` Just N
+      parse orientationReader "S" `shouldBe` Just S
+      parse orientationReader "W" `shouldBe` Just W
+      parse orientationReader "" `shouldBe` Nothing
+      parse orientationReader "X" `shouldBe` Nothing
+    it "parses coordinate correctly" $ do
+      parse coordinateReader "0 0" `shouldBe` Just (Coord 0 0)
+      parse coordinateReader "10 10" `shouldBe` Just (Coord 10 10)
+      parse coordinateReader "a b" `shouldBe` Nothing
+    it "parses an instruction correctly" $ do
+      parse instructionReader "M" `shouldBe` Just IMove
+      parse instructionReader "L" `shouldBe` Just ILeft
+      parse instructionReader "R" `shouldBe` Just IRight
+    it "parses a Rover correctly" $ do
+      parse parseRover "1 2 N" `shouldBe` Just (Rover (Coord 1 2) N)
+      parse parseRover "1 X N" `shouldBe` Nothing
+    it "parses instruction lists" $ do
+      parse (parseInstructions <* eof) "MMLR" `shouldBe` Just [IMove, IMove, ILeft, IRight]
+    it "parses problem" $ do
+      parse problemReader problem `shouldBe` Just [(Rover (Coord 1 2) N, [ILeft, IMove, ILeft, IMove, ILeft, IMove, ILeft, IMove, IMove])]
+    it "moves the rover" $ do
+      moveRover (Rover (Coord 0 0) N) IMove `shouldBe` Rover (Coord {x = 0, y = 1}) N
+      moveRover (Rover (Coord 0 0) S) IMove `shouldBe` Rover (Coord {x = 0, y = -1}) S
+      moveRover (Rover (Coord 0 0) E) IMove `shouldBe` Rover (Coord {x = 1, y = 0}) E
+      moveRover (Rover (Coord 0 0) W) IMove `shouldBe` Rover (Coord {x = -1, y = 0}) W
+      moveRover (Rover (Coord 0 0) N) IRight `shouldBe` Rover (Coord {x = 0, y = 0}) E
+      moveRover (Rover (Coord 0 0) N) ILeft `shouldBe` Rover (Coord {x = 0, y = 0}) W
 
-      it "runs the many rovers across mars" $ do
-        runRovers [
-            ((Rover (Coord 0 0) N), [IMove]),
-            ((Rover (Coord 0 0) N), [ILeft, IMove])
-          ] `shouldBe` [
-            (Rover (Coord { x = 0, y = 1}) N),
-            (Rover (Coord { x = -1, y = 0}) W)
-          ]
+    it "runs the rover across mars" $ do
+      runRover (Rover (Coord 0 0) N) [IMove] `shouldBe` Rover (Coord {x = 0, y = 1}) N
+      runRover (Rover (Coord 0 0) N) [ILeft, IMove] `shouldBe` Rover (Coord {x = -1, y = 0}) W
 
+    it "runs the many rovers across mars" $ do
+      runRovers
+        [ (Rover (Coord 0 0) N, [IMove]),
+          (Rover (Coord 0 0) N, [ILeft, IMove])
+        ]
+        `shouldBe` [ Rover (Coord {x = 0, y = 1}) N,
+                     Rover (Coord {x = -1, y = 0}) W
+                   ]
+
+    it "runs the examples" $ do
+      runRovers <$> parseProblem problem `shouldBe` Just [
+                                                            Rover (Coord 1 3) N
+                                                         ]
+
+-- * x = Rover
 -- 5 5
 -- 1 2 N
 -- LMLMLMLMM
