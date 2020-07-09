9b1134c1a7532d83b39ae9b5a4e417b27ea602c3
Andreas Rammhold <andreas@rammhold.de>
2020-07-09 13:08:13 +0200

    "finish" OCaml implementation (*cough cough*)

--- mars-rover/ocaml/rover.ml
+++ mars-rover/ocaml/rover.ml
@@ -20,7 +20,6 @@ type rover = { x : int; y : int; o : orientation }
 
 type program = rover * instruction list
 
-
 let string_of_rover r =
   String.concat " "
     [
@@ -33,29 +32,20 @@ let string_of_rover r =
       "}";
     ]
 
+let string_of_rovers is = String.concat " " (List.map string_of_rover is)
+
 let string_of_instruction i =
-  match i with
-    | Move -> "Move"
-    | Left -> "Left"
-    | Right -> "Right"
+  match i with Move -> "Move" | Left -> "Left" | Right -> "Right"
 
 let string_of_instructions is =
-  String.concat " "
-  (List.map string_of_instruction is)
+  String.concat " " (List.map string_of_instruction is)
 
 let string_of_program p =
-  String.concat " "
-  [
-    string_of_rover (fst p);
-    string_of_instructions (snd p);
-]
+  String.concat " " [ string_of_rover (fst p); string_of_instructions (snd p) ]
 
-let string_of_programs ps =
-  String.concat " "
-  (List.map string_of_program ps)
+let string_of_programs ps = String.concat " " (List.map string_of_program ps)
 
-
-let make_rover x y o = { x = x; y = y; o = o }
+let make_rover x y o = { x; y; o }
 
 let rotate_left r =
   match r.o with
@@ -87,35 +77,71 @@ let exec_command r i =
 let run_rover (r : rover) (insts : instruction list) =
   List.fold_left exec_command r insts
 
+let uncurried f (a, b) = f a b
+
+let run_programs (ps : program list) =
+  List.map (fun x -> uncurried run_rover x) ps
+
 (* -------------------------------------------------------------------------- *)
 (* Parser *)
 (* -------------------------------------------------------------------------- *)
 
 let mk_tuple a b = (a, b)
-let parse_int = take_while1 (function '0' .. '9' -> true | _ -> false) >>| int_of_string
-let parse_orientation = choice [char 'N' *> return N; char 'S' *> return S; char 'W' *> return W; char 'E' *> return E]
-let parse_instruction = choice [string "L" *> return Left; string "R" *> return Right; string "M" *> return Move ]
+
+let parse_int =
+  take_while1 (function '0' .. '9' -> true | _ -> false) >>| int_of_string
+
+let parse_orientation =
+  choice
+    [
+      char 'N' *> return N;
+      char 'S' *> return S;
+      char 'W' *> return W;
+      char 'E' *> return E;
+    ]
+
+let parse_instruction =
+  choice
+    [
+      string "L" *> return Left;
+      string "R" *> return Right;
+      string "M" *> return Move;
+    ]
+
 let parse_instructions = many parse_instruction
-let parse_coordinate = mk_tuple <$> parse_int <*> (char ' ' *> parse_int)
-let parse_rover = make_rover <$> parse_int <*> (char ' ' *> parse_int) <*> (char ' ' *> parse_orientation)
-let parse_program = parse_coordinate *> char '\n' *> many1 ((mk_tuple <$> parse_rover <*> (char '\n' *> parse_instructions)) <* char '\n')
+
+let parse_coordinate = mk_tuple <$> parse_int <*> char ' ' *> parse_int
+
+let parse_rover =
+  make_rover <$> parse_int
+  <*> char ' ' *> parse_int
+  <*> char ' ' *> parse_orientation
+
+let parse_program =
+  parse_coordinate *> char '\n'
+  *> many1
+       ( mk_tuple <$> parse_rover
+       <*> char '\n' *> parse_instructions
+       <* char '\n' )
 
 (* -------------------------------------------------------------------------- *)
 (* Tests *)
 (* -------------------------------------------------------------------------- *)
 
 let assert_rover_eq r1 r2 = assert_equal ~printer:string_of_rover r1 r2
-let assert_instructions_eq i1 i2 = assert_equal ~printer:string_of_instructions i1 i2
+
+let assert_instructions_eq i1 i2 =
+  assert_equal ~printer:string_of_instructions i1 i2
 
 let test_parse_orientation _ =
-    let n = parse_string  parse_orientation "N" in
-    let s = parse_string  parse_orientation "S" in
-    let e = parse_string  parse_orientation "E" in
-    let w = parse_string  parse_orientation "W" in
-    assert_equal n (Ok N);
-    assert_equal s (Ok S);
-    assert_equal e (Ok E);
-    assert_equal w (Ok W)
+  let n = parse_string parse_orientation "N" in
+  let s = parse_string parse_orientation "S" in
+  let e = parse_string parse_orientation "E" in
+  let w = parse_string parse_orientation "W" in
+  assert_equal n (Ok N);
+  assert_equal s (Ok S);
+  assert_equal e (Ok E);
+  assert_equal w (Ok W)
 
 let test_parse_instruction _ =
   let l = parse_string parse_instruction "L" in
@@ -131,25 +157,27 @@ let test_parse_instructions _ =
   assert_equal res (Ok [ Left; Right; Move; Move; Right; Left ])
 
 let test_parse_coordinate _ =
- let a = parse_string parse_coordinate "1 1" in
- let b = parse_string parse_coordinate "0 2" in
- assert_equal a (Ok (1, 1));
- assert_equal b (Ok (0, 2))
+  let a = parse_string parse_coordinate "1 1" in
+  let b = parse_string parse_coordinate "0 2" in
+  assert_equal a (Ok (1, 1));
+  assert_equal b (Ok (0, 2))
 
-let test_parse_program  _ =
+let test_parse_program _ =
   let p = "5 5\n1 2 N\nLRMRLM\n1 1 N\nLRM\n" in
   let res = Result.get_ok (parse_string parse_program p) in
-  let expected = [
-    ((make_rover 1 2 N), [Left; Right; Move; Right; Left; Move; ]);
-    ((make_rover 1 1 N), [Left; Right; Move; ]);
-  ] in
+  let expected =
+    [
+      (make_rover 1 2 N, [ Left; Right; Move; Right; Left; Move ]);
+      (make_rover 1 1 N, [ Left; Right; Move ]);
+    ]
+  in
   assert_equal ~printer:string_of_programs expected res
 
 let test_parse_rover _ =
   let r1 = parse_string parse_rover "1 2 N" in
-  let r1_expected = (Ok (make_rover 1 2 N)) in
+  let r1_expected = Ok (make_rover 1 2 N) in
   let r2 = parse_string parse_rover "0 8 E" in
-  let r2_expected = (Ok (make_rover 0 8 E)) in
+  let r2_expected = Ok (make_rover 0 8 E) in
   assert_equal r1 r1_expected;
   assert_equal r2 r2_expected
 
@@ -174,6 +202,14 @@ let test_exec _ =
   assert_rover_eq (exec_command r Left) { r with o = W };
   assert_rover_eq (exec_command r Right) { r with o = E }
 
+let test_run_programs _ =
+  let progs =
+    [ (make_rover 1 0 N, [ Left; Move ]); (make_rover 1 2 N, [ Right; Move ]) ]
+  in
+  let res = run_programs progs in
+  let expected = [ make_rover 0 0 W; make_rover 2 2 E ] in
+  assert_equal expected ~printer:string_of_rovers res
+
 let suite =
   "ExampleTestList"
   >::: [
@@ -181,11 +217,12 @@ let suite =
          "test_move" >:: test_move_rover;
          "test_rotate" >:: test_rotate;
          "test_exec" >:: test_exec;
+         "test_run_programs" >:: test_run_programs;
          "test_parse_orientation" >:: test_parse_orientation;
          "test_parse_instruction" >:: test_parse_instruction;
          "test_parse_coordinate" >:: test_parse_coordinate;
          "test_parse_rover" >:: test_parse_rover;
-         "test_parse_program" >:: test_parse_program
+         "test_parse_program" >:: test_parse_program;
        ]
 
 let () = run_test_tt_main suite
