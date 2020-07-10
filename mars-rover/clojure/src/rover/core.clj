71898b3b14e50a8f4029fb7caf4413f665c2d8a1
Tobias Pflug <tobias.pflug@gmail.com>
2020-07-10 16:55:05 +0200

    Complete Clojure code

--- mars-rover/clojure/src/rover/core.clj
+++ mars-rover/clojure/src/rover/core.clj
@@ -1,5 +1,11 @@
 (ns rover.core
+  (:require [clojure.string :as str ])
   (:gen-class))
+
+(defrecord Program [w h rover inst])
+
+(defn make-program [w h rover inst]
+  (->Program w h rover inst))
 
 (defrecord Rover [x y dir])
 
@@ -38,6 +44,40 @@
 
 (defn rover-exec-instrs [r is]
   (reduce rover-exec-instr r is))
+
+(defn parse-dir [c]
+  (cond
+    (or (= c \N) (= c "N")) :N
+    (or (= c \S) (= c "S")) :S
+    (or (= c \E) (= c "E")) :E
+    (or (= c \W) (= c "W")) :W))
+
+(defn parse-inst [i]
+  (cond
+    (or (= i \L) (= i "L")) :left
+    (or (= i \R) (= i "R")) :right
+    (or (= i \M) (= i "M")) :move))
+
+(defn parse-rover [s]
+  (let [xs (str/split s #" ")]
+    (make-rover (read-string (nth xs 0)) (read-string (nth xs 1)) (parse-dir (nth xs 2)))))
+
+(defn parse-program [lines]
+  (let [size (str/split (nth lines 0) #" ")
+        r (parse-rover (nth lines 1))
+        is (map parse-inst (seq (nth lines 2)))
+        w (read-string (nth size 0))
+        h (read-string (nth size 1))]
+    (make-program w h r is)))
+
+(defn parse-programs [s]
+  (let [lines (str/split-lines s)
+        ps (partition 3 lines)]
+    (map parse-program ps)))
+
+(defn run-program [text]
+  (let [ps (parse-programs text)]
+    (map (fn [p] (rover-exec-instrs (:rover p) (:inst p))) ps)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- mars-rover/clojure/test/rover/core_test.clj
+++ mars-rover/clojure/test/rover/core_test.clj
@@ -47,3 +47,34 @@
          (rover-exec-instrs (make-rover 1 1 :N) [:move, :move, :move]) (make-rover 1 4 :N))
          (rover-exec-instrs (make-rover 1 1 :N) [:left, :left, :left, :left]) (make-rover 1 1 :N)
          (rover-exec-instrs (make-rover 1 2 :N) [:left, :move, :left, :move, :left, :move, :left, :move, :move]) (make-rover 1 3 :N)))
+
+(deftest parse-program-test
+  (testing "parsing a program text"
+    (are [x y] (= x y)
+         (parse-program ["5 5", "1 2 N", "LMLMLMLMM"]) (make-program 5 5 (make-rover 1 2 :N) [:left, :move, :left, :move, :left, :move, :left, :move, :move]))))
+
+(deftest run-program-test
+  (testing "running a full program"
+    (are [x y] (= x y)
+         (run-program "5 5\n1 2 N\nLMLMLMLMM") [(make-rover 1 3 :N)]
+         (run-program "5 5\n1 2 N\nLMLMLMLMM\n5 5\n1 2 N\nLMLMLMLMM") [(make-rover 1 3 :N) (make-rover 1 3 :N)])))
+
+(deftest parse-dir-test
+  (testing "parsing directions"
+    (are [x y] (= x y)
+         (parse-dir \N) :N
+         (parse-dir \S) :S
+         (parse-dir \W) :W
+         (parse-dir \E) :E)))
+
+(deftest parse-inst-test
+  (testing "parsing instructions"
+    (are [x y] (= x y)
+         (parse-inst "L") :left
+         (parse-inst "R") :right
+         (parse-inst "M") :move)))
+
+(deftest parse-rover-test
+  (testing "parse rover"
+    (are [x y] (= x y)
+         (parse-rover "1 2 N") (make-rover 1 2 :N))))
